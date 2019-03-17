c3b337d9f61fdcf284713da653fdf3d87a480198
takumatt <takuma.matt@gmail.com>
2019-03-17 23:03:29 +0900

    Initial Commit

---
+++ Xciel/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Xciel
+//
+//  Created by Takuma Matsushita on 2019/03/17.
+//  Copyright © 2019 Takuma Matsushita. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
