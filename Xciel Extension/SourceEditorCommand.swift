f4f235c6a033bdbe46a4c0b4665f81ad31cd53c6
takumatt <takuma.matt@gmail.com>
2019-03-17 23:03:48 +0900

    init

---
+++ Xciel Extension/SourceEditorCommand.swift
@@ -0,0 +1,20 @@
+//
+//  SourceEditorCommand.swift
+//  Xciel Extension
+//
+//  Created by Takuma Matsushita on 2019/03/17.
+//  Copyright © 2019 Takuma Matsushita. All rights reserved.
+//
+
+import Foundation
+import XcodeKit
+
+class SourceEditorCommand: NSObject, XCSourceEditorCommand {
+    
+    func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) -> Void {
+        // Implement your command here, invoking the completion handler when done. Pass it nil on success, and an NSError on failure.
+        
+        completionHandler(nil)
+    }
+    
+}
