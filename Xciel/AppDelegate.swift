dd89d1cd3c231f46b5595abb1e2f3472bb73103a
takumatt <takuma.matt@gmail.com>
2019-04-19 18:08:00 +0900

    add button to open system preferences

--- Xciel/AppDelegate.swift
+++ Xciel/AppDelegate.swift
@@ -8,23 +8,53 @@
 
 import Cocoa
 
-@NSApplicationMain
+// @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    @IBOutlet weak var window: NSWindow!
+    // @IBOutlet weak var window: NSWindow!
+    
+//    private let window: NSWindow = {
+//        let viewController = ViewController()
+//        let window = NSWindow(contentViewController: viewController)
+//        window.title = "Xciel"
+//        return window
+//    }()
+    
+    private var window: NSWindow!
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
         
-        window.contentView = ViewController().view
+        let viewController = ViewController()
+        
+        self.window = NSWindow(
+            contentRect: .zero,
+            styleMask: [
+                .closable,
+                .titled,
+                .fullSizeContentView
+            ],
+            backing: .buffered,
+            defer: false
+        )
+        
+        self.window.title = "Xciel"
+        self.window.titlebarAppearsTransparent = false
+        self.window.titleVisibility = .visible
+        self.window.contentViewController = viewController
+        
+        self.window.center()
+        self.window.makeKeyAndOrderFront(nil)
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
 }
 
 final class ViewController: NSViewController {
@@ -33,11 +63,12 @@ final class ViewController: NSViewController {
         let button = NSButton()
         button.translatesAutoresizingMaskIntoConstraints = false
         button.stringValue = "HOge"
+        button.action = #selector(tap)
         return button
     }()
     
     override func loadView() {
-        self.view = NSView()
+        self.view = NSView(frame: .init(x: 0, y: 0, width: 300, height: 300))
     }
     
     override func viewDidLoad() {
@@ -50,5 +81,14 @@ final class ViewController: NSViewController {
             button.leftAnchor.constraint(equalTo: self.view.leftAnchor)
         ])
     }
+    
+    @objc
+    func tap() {
+        
+        let url = URL(fileURLWithPath: "/System/Library/PreferencePanes/Extensions.prefPane")
+        
+        if NSWorkspace.shared.open(url) {
+            print("default browser was successfully opened")
+        }
+    }
 }
-
---
+++ Xciel/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  Xciel
+//
+//  Created by Takuma Matsushita on 2019/04/19.
+//  Copyright © 2019 Takuma Matsushita. All rights reserved.
+//
+
+import Cocoa
+
+let delegate = AppDelegate()
+NSApplication.shared.delegate = delegate
+
+NSApplicationMain(CommandLine.argc, CommandLine.unsafeArgv)
